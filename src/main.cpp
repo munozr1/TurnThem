5f860ab819352e716f62c738987b650bf3d2cdfc
Rodrigo Munoz <rm117579@gmail.com>
2025-06-18 22:49:53 -0500

    add drag and drop for weapon cards

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,6 @@ struct Projectile {
     float angle;
 };
 
-
 struct GameObject {
     public:
         virtual void update() = 0;
@@ -21,12 +20,19 @@ struct GameObject {
 
 struct WeaponCard : public GameObject {
     public:
-        WeaponCard(Texture2D& sprite_sheet, float sheet_size, float width, float height):
-            frameWidth(width),
-            frameHeight(height),
+        WeaponCard(Texture2D& sprite_sheet, float sheet_size, float width, float height, float x, float y):
+            position((Vector2){x, y}),
+            frame_width(width),
+            frame_height(height),
             sprite_sheet(sprite_sheet),
             frame((Rectangle){150, 0, width, height}){}
         void setxy(Vector2 pos){position = pos;}
+        bool isPointInside(Vector2 point){
+            return point.x >= position.x &&
+                point.x <= position.x + frame_width &&
+                point.y >= position.y &&
+                point.y <= position.y + frame_height;
+        }
         /*
          * Check if we are being moved around
          */
@@ -38,21 +44,21 @@ struct WeaponCard : public GameObject {
         Texture2D& sprite_sheet;
         Rectangle frame;
         Vector2 position;
-        int frameWidth;
-        int frameHeight;
+        int frame_width;
+        int frame_height;
 };
 
 uint64_t frame_counter = 0;
 
 struct Weapon : public GameObject {
     public:
-        Weapon(Texture2D& sprite_sheet, float width, float height, int fpu, int sprites):
+        Weapon(Texture2D& sprite_sheet, float width, float height, int fpu, int sprites, float x, float y):
             sprite_sheet(sprite_sheet),
             sprites(sprites),
             animate(true),
             frame_width(width),
             frame_height(height),
-            position((Vector2){200, 200}),
+            position((Vector2){x, y}),
             frame((Rectangle){0,0,width,height}),
             frames_per_update(fpu),
             current_frame(0){};
@@ -81,10 +87,19 @@ struct Weapon : public GameObject {
         bool animate;
 };
 
+enum MouseState {
+    NORMAL,
+    DRAGGING,
+    DROP
+};
+
 int main(void)
 {
     // Initialization
     std::vector<GameObject*> game_objects;
+    std::vector<WeaponCard*> weapon_cards;
+    MouseState mouse_state = MouseState::NORMAL;
+    WeaponCard* dragging_card;
     //--------------------------------------------------------------------------------------
     const int screenWidth = 520;
     const int screenHeight = 800;
@@ -93,15 +108,27 @@ int main(void)
     InitWindow(screenWidth, screenHeight, "TurnThem");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
+    //--------------------------------------------------------------------------------------
+    
+    Texture2D cannon_sprite_sheet = LoadTexture("assets/cannon_sprite_sheet.png");
     Texture2D scannon_sprite_sheet = LoadTexture("assets/scannon_sprite_sheet.png");
-    Weapon scannon(scannon_sprite_sheet, 50.0f, 65.0f, 9, 3);
-    game_objects.push_back(&scannon);
+
     //--------------------------------------------------------------------------------------
 
-    WeaponCard scannon_card(scannon_sprite_sheet, 150, 90.0f, 120.0f);
+    Weapon scannon(scannon_sprite_sheet, 50.0f, 65.0f, 9, 3, 200, 200);
+    game_objects.push_back(&scannon);
+    Weapon cannon(cannon_sprite_sheet, 50.0f, 65.0f, 13, 3, 200, 280);
+    game_objects.push_back(&cannon);
+    
+    //--------------------------------------------------------------------------------------
+    WeaponCard scannon_card(scannon_sprite_sheet, 150, 90.0f, 120.0f, 28, 653);
     game_objects.push_back(&scannon_card);
+    WeaponCard cannon_card(cannon_sprite_sheet, 150, 90.0f, 120.0f, 153, 653);
+    game_objects.push_back(&cannon_card);
+
+    weapon_cards.push_back(&scannon_card);
+    weapon_cards.push_back(&cannon_card);
     //--------------------------------------------------------------------------------------
 
     Texture2D deck_sprite_sheet = LoadTexture("assets/deck_sprite_sheet.png");
@@ -115,22 +142,42 @@ int main(void)
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        frame_counter++;
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-        /*
-        frame_counter++;
-
-        if (frame_counter >= frames_per_update){
-            frame_counter = 0;
-            current_frame++;
-            cannon_frame.x = frameWidth*(current_frame % 3);
+        
+        switch (mouse_state) {
+            case MouseState::NORMAL:
+                std::cout << "normal" << std::endl;
+                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                    /* check if the mouse is hovering over a card */
+                    Vector2 mouse_pos = GetMousePosition();
+                    for (auto card : weapon_cards){
+                        if (card->isPointInside(mouse_pos)){
+                            mouse_state = MouseState::DRAGGING;
+                            dragging_card = card;
+                            break;
+                        }
+                    }
+                }
+                break;
+            case MouseState::DRAGGING:{
+                Vector2 mouse_pos = GetMousePosition();
+                dragging_card->setxy(mouse_pos);
+                if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)){
+                    mouse_state = MouseState::NORMAL;
+                }
+                break;
+                }
+            case MouseState::DROP:
+                /* TODO: handle dropping card */ 
+                /* if dropped inside deck, reset original position
+                 * if dropped on play area, remove from weapon_cards array and add the weapon 
+                 * to the play area.
+                 */
+            default:
+                break;
         }
-        */
-        // Draw
+
         //----------------------------------------------------------------------------------
+        frame_counter++;
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
